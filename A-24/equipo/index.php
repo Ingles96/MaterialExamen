fa96a51fae164f07077a9897e0129c5dd634f0f3
Ingles96 <josepingles96@gmail.com>
2017-05-11 00:24:02 +0200

    A24

---
+++ A-24/equipo/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Index pagina gestion NBA</title>
+  </head>
+  <body>
+    <h3><a href="insertarequipo.php"> INSERTAR UN NUEVO EQUIPO</a></h3>
+    <br>
+    <br>
+  </body>
+</html>
---
+++ A-24/jugador/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Index pagina gestion NBA</title>
+  </head>
+  <body>
+  
+    <br>
+    <br>
+    <h3><a href="insertarjugador.php">INSERTAR UN NUEVO JUGADOR</a></h3>
+  </body>
+</html>
